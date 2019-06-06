22084e28e320612450041da7086a5fbf9bfa57ca
nitayin <51431789+nitayin@users.noreply.github.com>
2019-06-06 11:09:23 -0400

    Update notesController.php

--- app/Http/Controllers/notesController.php
+++ app/Http/Controllers/notesController.php
@@ -12,6 +12,12 @@ class notesController extends Controller
    function store(Request $request){
         $notes = new notes($request->all());
         
-        $
+        $quantity=3;
+        $notes->first_partial;
+        $notes->second_partial;
+        $notes->third_partial;
+        $notes->final_note=($notes->first_partial + $notes->second_partial + $notes->third_partial)/$quantity;
+        $notes->save();
+        return $notes;
    }
 }
